14ec2bc4067ead0e2679f3371f5485015682ce53
HariharanPandurengan <hariharanpandurengan@gmail.com>
2024-11-08 15:23:52 +0530

    Update UrlGen.jsx

--- src/components/UrlGen.jsx
+++ src/components/UrlGen.jsx
@@ -6,43 +6,45 @@ import { useNavigate } from "react-router-dom";
 function UrlGen() {
   
     const[qr2,setQr2] = useState('')
-    const[qr3,setQr3] = useState('')
+    // const[qr3,setQr3] = useState('')
 
-    const navigate  = useNavigate()
+    // const navigate  = useNavigate()
+
     const encryptString = (string, secretKey) => {
       return CryptoJS.AES.encrypt(string, secretKey).toString();
     };
     
     const secretKey = 'url';
-    const originalString = 'https://www.xorostory.com';
+    // const originalString = 'https://www.xorostory.com';
 
-    const encryptedString = encryptString(originalString, secretKey);
+    // const encryptedString = encryptString(originalString, secretKey);
     const encryptedString2 = encryptString(qr2, secretKey);
 
   
-    const encodedString = encodeURIComponent(encryptedString);
+    // const encodedString = encodeURIComponent(encryptedString);
     const encodedString2 = encodeURIComponent(encryptedString2);
 
-    sessionStorage.setItem('hiddenParam',qr3)
-    useEffect(()=>{
-      sessionStorage.removeItem('hiddenParam')
-      sessionStorage.setItem('hiddenParam',qr3)
-    },[qr3])
+    // sessionStorage.setItem('hiddenParam',qr3)
+    // useEffect(()=>{
+    //   sessionStorage.removeItem('hiddenParam')
+    //   sessionStorage.setItem('hiddenParam',qr3)
+    // },[qr3])
+
     return (
       <section style={{display:'flex',justifyContent:'space-around',alignItems:'center',width:'100%'}}>
-        <div style={{marginRight:'50px'}}>
+        {/* <div style={{marginRight:'50px'}}>
           <QRCode value={`https://qr3-eta.vercel.app/unMask/${encodedString}`} />
-        </div>
+        </div> */}
         <div style={{marginRight:'50px'}}> 
           <QRCode value={`https://qr3-eta.vercel.app/A/${encodedString2}`} /> <br></br>
           <input placeholder="Enter the value to send" onChange={(e)=>setQr2(e.target.value)}/>
         </div>
-        <div>
+        {/* <div>
           <button style={{backgroundColor:'green',color:'white'}} onClick={()=>{
             navigate('/B')
           }}>click here to view 3D model</button> <br></br>
           <input placeholder="Enter the value to send" onChange={(e)=>setQr3(e.target.value)}/>
-        </div>
+        </div> */}
       </section>
     )
   }
